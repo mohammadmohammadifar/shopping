010db3548bb036678d446eb19d5ba33f07face3b
mohammad <mo.mohammadifar@gmail.com>
2024-08-24 02:38:39 -0700

    init

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BrandController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::prefix('/admin-panel')->group(
+    function () {
+        Route::resource('/brands', BrandController::class);
+    }
+);
