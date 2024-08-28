742a1d61b24650f77fafbe4ddfd6f5e701e03820
mohammad <mo.mohammadifar@gmail.com>
2024-08-28 10:36:25 -0700

    add attribute_category

---
+++ database/migrations/2024_08_28_125257_create_attribute_category_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attribute_category', function (Blueprint $table) {
+
+            $table->foreignId('attribute_id');
+            $table->foreign('attribute_id')->references('id')->on('attributes')->onDelete('cascade');
+
+            $table->foreignId('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+
+            $table->boolean('is_filter');
+
+            $table->boolean('is_variation');
+
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attribute_category');
+    }
+};
